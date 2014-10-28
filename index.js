3f44f1ed9069366ef32a7d737f18a4ade5ff57d4
Luis Albertorio <luisalbertorio21@gmail.com>
2014-10-28 23:52:00 +0000

    changed index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
+var path = require('path');
+
+var filepath = path.join(__dirname, 'index.html');
+
+var qwerty = fs.readFile(filepath,{encoding:'utf-8'}, function(err,data){
+	if (!err){
+    	console.log('received data: ' + data);
+    	response.writeHead(200, {'Content-Type': 'text/html'});
+    	response.write(data);
+    	response.end();
+    	}else{
+        	console.log(err);
+    	}
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+  response.send(qwerty)
 })
 
 app.listen(app.get('port'), function() {
